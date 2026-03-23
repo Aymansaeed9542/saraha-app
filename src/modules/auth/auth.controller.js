32d1eb3795b43a92b40587965b92596188f0860b
Aymansaeed9542 <aym9542@gmail.com>
2026-03-23 12:37:43 +0200

    feat: add joi validation and reusable middleware

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,21 +1,23 @@
 import { Router } from "express";
 import { logIn, signUp } from "./auth.service.js";
 import { SuccessResponse } from "../../common/utils/response/success.responce.js";
+import { validation } from "../../common/utils/validation/validation.js";
+import { loginSchema, signupSchema } from "./validation/auth.validation.js";
 
 
 const router = Router() 
 // route to sign upu a new user
-router.post('/signup',async(req,res)=>{
+router.post('/signup', validation(signupSchema), async (req, res) => {
     const addUser = await signUp(req.body)
     // return a success response with the added user data
-    return SuccessResponse({res, message:'user added successfully',status:201,data: addUser})
+    return SuccessResponse({ res, message: 'user added successfully', status: 201, data: addUser })
 })
 
 // route to log in a user
-router.post('/login', async(req,res)=>{
+router.post('/login', validation(loginSchema), async (req, res) => {
     const user = await logIn(req.body)
 
-    return SuccessResponse({res, message:'user logged in successfully',status:200,data: user})
+    return SuccessResponse({ res, message: 'user logged in successfully', status: 200, data: user })
 })
 
 
